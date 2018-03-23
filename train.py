b1dd08c897fbc2454b4aece53a4f534e06e7fd8e
hdriqi <riqi.asdf@gmail.com>
2018-03-23 19:55:14 +0700

    optimizer using sgd

--- train.py
+++ train.py
@@ -9,14 +9,14 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # Global config variables
 file_name = "./model"
 num_steps = 3
-epoch = 20
+epoch = 40
 days_predict = 2
 data_length = 1000
 input_dimension = 14
 output_dimension = 2
 batch_size = 128
 num_classes = 2
-state_size = 20
+state_size = 40
 learning_rate = 0.01
 
 def prepare_train_data():
@@ -48,7 +48,7 @@ test_input, test_output = prepare_test_data()
 
 x = tf.placeholder(tf.float32, [None, None, input_dimension], name='input_placeholder')
 y = tf.placeholder(tf.float32, [None, None, output_dimension], name='labels_placeholder')
-init_state = tf.placeholder(tf.float32, [None, state_size])
+init_state = tf.placeholder(tf.float32, [None, state_size], name='state_placeholder')
 
 cell = tf.contrib.rnn.BasicRNNCell(state_size, activation=tf.nn.relu)
 cell = tf.nn.rnn_cell.DropoutWrapper(cell=cell, output_keep_prob=0.5)
@@ -58,7 +58,7 @@ val = tf.transpose(val, [1, 0, 2])
 rnn_last = tf.gather(val, num_steps - 1)
 
 weight = tf.Variable(tf.truncated_normal([state_size, num_classes]))
-bias = tf.Variable(tf.constant(0.1, shape=[num_classes]))
+bias = tf.Variable(tf.zeros(num_classes))
 
 logits = tf.matmul(rnn_last, weight) + bias
 prediction = tf.nn.softmax(logits)
@@ -69,7 +69,10 @@ y_last = tf.gather(y_last, num_steps - 1)
 losses = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y_last, logits=logits)
 
 avg_losses =  tf.reduce_mean(losses)
-train_step = tf.train.AdamOptimizer(learning_rate).minimize(avg_losses)
+train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(avg_losses)
+# train_step = tf.train.AdadeltaOptimizer(learning_rate).minimize(avg_losses)
+# train_step = tf.train.RMSPropOptimizer(learning_rate).minimize(avg_losses)
+# train_step = tf.train.AdamOptimizer(learning_rate).minimize(avg_losses)
 
 correct = tf.equal(tf.argmax(y_last, 1), tf.argmax(prediction, 1))
 accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
@@ -112,54 +115,59 @@ def train_network():
                 })
             
             test_losses.append(avg_losses_)
-        print('Days to predict:', days_predict, 'Epoch:', step, "Hidden Node:", state_size, "Timesteps:", num_steps, "Accuracy", accuracy_ * 100)
+        # print('Days to predict:', days_predict, 'Epoch:', step, "Hidden Node:", state_size, "Timesteps:", num_steps, "Accuracy", accuracy_ * 100)
         return accuracy_ * 100, train_losses, test_losses
-
-def test_network():
-    test_input, test_output = prepare_test_data()
-    
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-        test_accuracy = []  
-        saver = tf.train.Saver()
-        saver.restore(sess, file_name)
-        accuracy_, correct_, y_last_, prediction_, batch_loss_ = sess.run([accuracy, correct, y_last, prediction, batch_loss], 
-            feed_dict={
-                x: test_input, 
-                y: test_output
-            })
-        print('Day to predict:', days_predict, 'Epoch:', step, "Hidden Node:", state_size, "Timesteps:", num_steps, "Accuracy", accuracy_ * 100)
-        test_accuracy.append(accuracy_*100)
-        return accuracy_*100, np.mean(train_losses), batch_loss_
 
 total_acc = []
 total_train_losses = []
 total_valid_losses = []
 
 # TIMESTEP_LOOP
-for i in range(1, 4):
-    num_steps = 2 * i + 1
-    total_acc.append([])
-    # DAYS_LOOP
-    for j in range(2, 61, 2):
-        days_predict = j
-        state_size = 20
-        acc, train_loss, val_loss = train_network()
-        total_acc[i - 1].append(acc)
-        total_train_losses.append(train_loss)
-        total_valid_losses.append(val_loss)
+for z in range(1):
+    for i in range(1, 2):
+        num_steps = 2 * i + 1
+        # total_acc.append([])
+        total_acc = []
+        total_train_losses = []
+        total_valid_losses = []
+        # DAYS_LOOP
+        for j in range(2, 61, 2):
+            days_predict = j
+            acc, train_loss, val_loss = train_network()
+            total_acc.append(acc)
+            total_train_losses.append(train_loss)
+            total_valid_losses.append(val_loss)
 
-# PRINT TRAIN LOSS
-# for i, val in enumerate(total_train_losses):
-#     plt.plot(val, label="Train Losses Index " + str(i))
+    # PRINT TRAIN LOSS
+    # for i, val in enumerate(total_train_losses):
+    #     plt.plot(val, label="Train Losses Index " + str(i))
 
-# PRINT VALID LOSS
-# for i, val in enumerate(total_valid_losses):
-#     plt.plot(val, label="Valid Losses Index " + str(i))
+    print("Average Loss", str(np.mean(total_valid_losses)))
+    total_valid_losses = np.array(total_valid_losses)
+    best_loss = np.argmin(total_valid_losses.transpose()[-1])
+    worst_loss = np.argmax(total_valid_losses.transpose()[-1])
+    print("Best Loss", str(best_loss * 2), "->", str(total_valid_losses.transpose()[-1][best_loss]))
+    print("Worst Loss", str(worst_loss * 2), "->", str(total_valid_losses.transpose()[-1][worst_loss]))
+    # PRINT VALID LOSS
+    # for i, val in enumerate(total_valid_losses):
+    #     plt.plot(val, label="Valid Losses Index " + str(i))
+    print()
 
-# PRINT ACCURACY
-for i, val in enumerate(total_acc):
-    plt.plot([i for i in range(2, 61, 2)], val, label="Accuracy - Timesteps -> " + str(2 * i + 1))
+    print("Average Accuracy", str(np.mean(total_acc)))
+    total_acc = np.array(total_acc)
+    best_acc = np.argmax(total_acc)
+    worst_acc = np.argmin(total_acc)
+    print("Best Accuracy", str(best_acc * 2), "->", str(total_acc.flatten()[best_acc]))
+    print("Worst Accuracy", str(worst_acc *2), "->", str(total_acc.flatten()[worst_acc]))
+    # PRINT ACCURACY
+    # for i, val in enumerate(total_acc):
+    plt.plot([i for i in range(2, 61, 2)], total_acc, label="Accuracy - Timesteps -> " + str(2 * i + 1))
 
-plt.legend()
-plt.show()
+    plt.yticks([i for i in range(0, 101, 10)])
+    plt.ylabel("Accuracy")
+    plt.xticks([i for i in range(2, 61, 2)])
+    plt.xlabel("Time Window (days)")
+    plt.legend()
+    plt.show()
+
+    print('------------------------')
