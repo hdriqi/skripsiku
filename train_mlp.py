f54fa54bb6af96debf5d2387b7bf6f1975b04c0d
Rahmat Albariqi <riqi@localhost.localdomain>
2018-04-21 17:57:46 +0700

    got result bitchez

--- train_mlp.py
+++ train_mlp.py
@@ -10,23 +10,61 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Global config variables
 feature_dimension = 14
-timesteps = 3
-input_dimension = feature_dimension * timesteps
 output_dimension = 2
-num_classes = 2
 data_length = 1300
 batch_size = 128
 learning_rate = 0.01
 k_fold = KFold(n_splits=5, shuffle=True)
-
-hidden_node_1 = int(sqrt((output_dimension+2)*input_dimension) + 2*sqrt(input_dimension/(output_dimension+2)))
-hidden_node_2 = int(output_dimension*sqrt(input_dimension/(output_dimension+2)))
 epoch = 50
-days_predict = 2
 
-def prepare_train_data(day):
+class Model:
+	def __init__(self, data, target, input_dimension):
+		hidden_node_1 = int(sqrt((output_dimension+2)*input_dimension) + 2*sqrt(input_dimension/(output_dimension+2)))
+		hidden_node_2 = int(output_dimension*sqrt(input_dimension/(output_dimension+2)))
+
+		x = data
+		y = target
+		hidden_1 = tf.layers.dense(inputs=x, units=hidden_node_1, activation=tf.nn.relu)
+		dropout_1 = tf.layers.dropout(hidden_1, rate=0.5)
+		hidden_2 = tf.layers.dense(inputs=dropout_1, units=hidden_node_2, activation=tf.nn.relu)
+		dropout_2 = tf.layers.dropout(hidden_2, rate=0.5)
+		logits = tf.layers.dense(inputs=dropout_2, units=output_dimension)
+
+		prediction = tf.nn.softmax(logits)
+
+		losses = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=logits)
+
+		self._cost =  tf.reduce_mean(losses)
+		self._optimize = tf.train.AdamOptimizer(learning_rate).minimize(self._cost)
+
+		self._recall = tf.metrics.recall(tf.argmax(y, 1), tf.argmax(prediction, 1))
+		self._precision = tf.metrics.precision(tf.argmax(y, 1), tf.argmax(prediction, 1))
+		self._accuracy = tf.metrics.accuracy(tf.argmax(y, 1), tf.argmax(prediction, 1))
+
+	@property
+	def cost(self):
+		return self._cost
+
+	@property
+	def optimize(self):
+		return self._optimize
+
+	@property
+	def recall(self):
+		return self._recall
+
+	@property
+	def precision(self):
+		return self._precision
+
+	@property
+	def accuracy(self):
+		return self._accuracy
+
+
+def prepare_train_data(days_predict, timesteps):
 	df_norm = pd.read_csv("dataset-normalize.csv").drop('timestamp', axis=1).as_matrix()[1:]
-	df_output = pd.read_csv("labels/" + str(day) + "days.csv").as_matrix()
+	df_output = pd.read_csv("labels/" + str(days_predict) + "days.csv").as_matrix()
 	train_input = []
 	train_output = []
 	for i in range(0, data_length):
@@ -35,34 +73,7 @@ def prepare_train_data(day):
 
 	return np.asarray(train_input), np.asarray(train_output)
 
-x = tf.placeholder(tf.float32, [None, input_dimension], name='input_placeholder')
-y = tf.placeholder(tf.float32, [None, output_dimension], name='labels_placeholder')
-
-hidden_1 = tf.layers.dense(inputs=x, units=hidden_node_1, activation=tf.nn.relu)
-dropout_1 = tf.layers.dropout(hidden_1, rate=0.5)
-hidden_2 = tf.layers.dense(inputs=dropout_1, units=hidden_node_2, activation=tf.nn.relu)
-dropout_2 = tf.layers.dropout(hidden_2, rate=0.5)
-logits = tf.layers.dense(inputs=dropout_2, units=num_classes)
-
-prediction = tf.nn.softmax(logits)
-
-losses = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=logits)
-
-cost =  tf.reduce_mean(losses)
-# train_step = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-train_step = tf.train.AdadeltaOptimizer(learning_rate).minimize(cost)
-# train_step = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
-# train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
-
-correct = tf.equal(tf.argmax(y, 1), tf.argmax(prediction, 1))
-recall = tf.metrics.recall(tf.argmax(y, 1), tf.argmax(prediction, 1))
-precision = tf.metrics.precision(tf.argmax(y, 1), tf.argmax(prediction, 1))
-accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
-
-def train_network(train_input, train_output, day, timesteps, save=True):
-	days_predict = day
-	timesteps = timesteps
-
+def train_network(train_input, train_output, days_predict, timesteps, save=True):
 	total_acc = []
 	total_train_losses = []
 	total_val_losses = []
@@ -71,6 +82,13 @@ def train_network(train_input, train_output, day, timesteps, save=True):
 
 	train_losses_by_epoch = []
 	val_losses_by_epoch = []
+
+	tf.reset_default_graph()
+
+	input_dimension = feature_dimension * timesteps
+	x = tf.placeholder(tf.float32, [None, input_dimension], name='input_placeholder')
+	y = tf.placeholder(tf.float32, [None, output_dimension], name='labels_placeholder')
+	model = Model(x, y, input_dimension)
 
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
@@ -99,16 +117,15 @@ def train_network(train_input, train_output, day, timesteps, save=True):
 					inp, out = train_input_kfold[ptr:ptr+batch_size], train_output_kfold[ptr:ptr		+batch_size]
 					inp = inp.reshape(batch_size, input_dimension)
 					ptr+=batch_size
-					training_step, training_cost = sess.run([train_step, cost], 
+					_, training_cost = sess.run([model.optimize, model.cost], 
 						feed_dict={
 							x: inp,
 							y: out
 						})
 					train_loss.append(training_cost)
 				
-				# RESULT PER EPOCH
 				val_input_kfold = val_input_kfold.reshape(len(val_indices), input_dimension)
-				accuracy_, precision_, recall_, prediction_, cost_ = sess.run([accuracy, precision, recall, prediction, cost], 
+				accuracy_, precision_, recall_, cost_ = sess.run([model.accuracy, model.precision, model.recall, model.cost], 
 					feed_dict={
 						x: val_input_kfold, 
 						y: val_output_kfold
@@ -116,7 +133,7 @@ def train_network(train_input, train_output, day, timesteps, save=True):
 
 				train_losses.append(np.mean(train_loss))
 				train_loss = []
-				val_accuracy.append(accuracy_*100)
+				val_accuracy.append(accuracy_[0]*100)
 				val_losses.append(cost_)
 				val_precision.append(precision_[0])
 				val_recall.append(recall_[0])
@@ -143,13 +160,24 @@ def train_network(train_input, train_output, day, timesteps, save=True):
 		f_recall = np.mean(total_val_recall)
 		f_fscore = 2*((f_precision*f_recall)/(f_precision+f_recall))
 
-		print("------------ Day", day, "• Timesteps", timesteps, "------------")
+		with open('mlp_result', 'a') as f:
+			print("------------ Day", days_predict, "• Timesteps", timesteps, "------------", file=f)
+			print("Average Acc {:.2f}".format(f_acc), file=f)
+			print("Average Train Loss", f_train_losses, file=f)
+			print("Average Validation Loss", f_val_losses, file=f)
+			print("Precision", f_precision, file=f)
+			print("Recall", f_recall, file=f)
+			print("F-Measure", f_fscore, file=f)
+			print("", file=f)
+
+		print("------------ Day", days_predict, "• Timesteps", timesteps, "------------")
 		print("Average Acc {:.2f}".format(f_acc))
 		print("Average Train Loss", f_train_losses)
 		print("Average Validation Loss", f_val_losses)
 		print("Precision", f_precision)
 		print("Recall", f_recall)
 		print("F-Measure", f_fscore)
+		print()
 
 		return f_acc, f_precision, f_recall, f_fscore, f_val_losses, f_train_losses
 
@@ -165,19 +193,18 @@ def visualize_val_loss(total_val_losses, timesteps):
 	plt.xticks([i for i in range(2, 61, 2)])
 	plt.xlabel("Time Window (days)")
 
-if __name__ == "__main__":
+def main():
 	top_acc = 0
 	top_acc_details = ""
-	for i in range(1, 2):
+	for i in range(1, 4):
 		timesteps = 2 * i + 1
-		hidden_node_1 = int(sqrt((output_dimension+2)*input_dimension) + 2*sqrt(input_dimension/(output_dimension+2)))
-		hidden_node_2 = int(output_dimension*sqrt(input_dimension/(output_dimension+2)))
+		
 		total_acc = []
 		total_losses = []
 		total_val_losses = []
 		for j in range(2, 61, 2):
-			train_input, train_output = prepare_train_data(j)
-			acc_, precision_, recall_, fscore_, val_losses_, train_losses_ = train_network(train_input=train_input, train_output=train_output, day=j, timesteps=timesteps, save=False)
+			train_input, train_output = prepare_train_data(j, timesteps)
+			acc_, precision_, recall_, fscore_, val_losses_, train_losses_ = train_network(train_input=train_input, train_output=train_output, days_predict=j, timesteps=timesteps, save=False)
 			
 			total_acc.append(acc_)
 			total_losses.append(train_losses_)
@@ -186,11 +213,18 @@ if __name__ == "__main__":
 			if(acc_ > top_acc):
 				top_acc = acc_
 				top_acc_details = "Days " + str(j) + " Timesteps " + str(timesteps)
-			print()
-		visualize_loss(total_losses, timesteps)
-		visualize_val_loss(total_val_losses, timesteps)
-		# plt.plot([z for z in range(2, 61, 2)], total_acc, label="Accuracy - Timesteps " + str(timesteps))
+			
+		plt.plot([z for z in range(2, len(total_acc), 2)], total_acc, label="Accuracy - Timesteps " + str(timesteps))
 
-		print(top_acc, top_acc_details)
-		plt.legend()
-		plt.show()
+	with open('mlp_result', 'a') as f:
+		print(top_acc, top_acc_details, file=f)
+
+	print(top_acc, top_acc_details)
+	plt.yticks([i for i in range(40, 81, 10)])
+	plt.ylabel("Accuracy")
+	plt.xticks([i for i in range(2, len(total_acc), 2)])
+	plt.xlabel("Time Window (days)")
+	plt.legend()
+	plt.show()
+
+main()
